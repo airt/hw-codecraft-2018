6c9d409794bded44dac40792eb5b84ae35cffc66
cutoutsy <cutoutsy@gmail.com>
2018-03-19 17:34:27 +0800

    add LinearRegression

---
+++ sdk-java/code/ecs/src/io/ecs/model/LinearRegressionTest.java
@@ -0,0 +1,20 @@
+package io.ecs.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LinearRegressionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void fit() {
+    }
+
+    @Test
+    void predict() {
+    }
+
+}

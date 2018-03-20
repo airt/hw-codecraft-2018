727926e1d2e323776344b61121a695c1339d3c3e
cutoutsy <cutoutsy@gmail.com>
2018-03-20 16:19:29 +0800

    generate train data: slide window(T)

--- sdk-java/code/ecs/src/io/ecs/generator/DataGenerator.java
+++ sdk-java/code/ecs/src/io/ecs/generator/DataGenerator.java
@@ -1,11 +1,15 @@
 package io.ecs.generator;
 
 import com.elasticcloudservice.predict.Record;
+import com.elasticcloudservice.predict.Server;
 import io.ecs.common.Matrix;
 import io.ecs.common.Tuple2;
 
+import java.util.List;
+import java.util.Map;
+
 public interface DataGenerator {
 
-  Tuple2<Matrix, Matrix> generate(Iterable<Record> records);
+    Map<String, Tuple2<Matrix, Matrix>> generate(List<Record> records, List<Server> flavors, int T);
 
 }

d00a631a257ae86af9d2ca0779dbd038be439055
airt <airtial@gmail.com>
2018-04-06 11:56:02 +0800

    feat: int tuple

--- sdk-java/code/ecs/src/io/ecs/common/IntTuple2.java
+++ sdk-java/code/ecs/src/io/ecs/common/IntTuple2.java
@@ -3,33 +3,33 @@ package io.ecs.common;
 import java.util.Objects;
 
 @SuppressWarnings("WeakerAccess")
-public class Tuple2<T1, T2> {
+public class IntTuple2 {
 
-    public static <T1, T2> Tuple2<T1, T2> of(T1 _1, T2 _2) {
-        return new Tuple2<>(_1, _2);
+    public static IntTuple2 of(int _1, int _2) {
+        return new IntTuple2(_1, _2);
     }
 
-    private final T1 _1;
-    private final T2 _2;
+    private final int _1;
+    private final int _2;
 
-    private Tuple2(T1 _1, T2 _2) {
+    private IntTuple2(int _1, int _2) {
         this._1 = _1;
         this._2 = _2;
     }
 
-    public T1 _1() {
+    public int _1() {
         return _1;
     }
 
-    public T2 _2() {
+    public int _2() {
         return _2;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Tuple2)) return false;
-        Tuple2<?, ?> t = (Tuple2<?, ?>) o;
+        if (!(o instanceof IntTuple2)) return false;
+        IntTuple2 t = (IntTuple2) o;
         return Objects.equals(_1, t._1) && Objects.equals(_2, t._2);
     }
 
